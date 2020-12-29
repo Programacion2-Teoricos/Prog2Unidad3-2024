eb54d5df1c0e83b0fe2bb822e848620b4b33e834
Elizabeth Izquierdo <elizabeth.izquierdo@ces.edu.uy>
2020-12-29 13:33:06 +0000

    Cambie el nombre de Main a PruebaEstudiantes y agregue botón run

--- PruebaEstudiantes.java
+++ PruebaEstudiantes.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import logica.*;
 
-public class Main {
+public class PruebaEstudiantes {
 	public static void main(String[] args) {
 			
 		Scanner entrada = new Scanner(System.in);
